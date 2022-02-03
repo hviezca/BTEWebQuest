a50cd715443ab1a13c4a175c59265e4113632030
hviezca <hviezca@my.gcu.edu>
2022-02-03 14:56:22 -0700

    Initial Project Creation

---
+++ BTEWebQuest/src/main/java/com/btewebquest/BteWebQuestApplication.java
@@ -0,0 +1,13 @@
+package com.btewebquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BteWebQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BteWebQuestApplication.class, args);
+	}
+
+}
---
+++ BTEWebQuest/src/test/java/com/btewebquest/BteWebQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btewebquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BteWebQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
