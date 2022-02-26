d1b9617902eb751c848de346a91ea9d638f9d5fc
sfradet <sfradet@hotmail.com>
2022-02-26 09:26:32 -0800

    Small change by Shawn that removed the console "Hello World" just to verify I have full access.

--- BTEWebQuest/src/main/java/com/btewebquest/BteWebQuestApplication.java
+++ BTEWebQuest/src/main/java/com/btewebquest/BteWebQuestApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BteWebQuestApplication {
 
-	public static void main(String[] args) {		
-		System.out.println("Hello World");
+	public static void main(String[] args) {
 		SpringApplication.run(BteWebQuestApplication.class, args);
 	}
 
