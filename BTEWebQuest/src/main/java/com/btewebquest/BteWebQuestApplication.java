7938cce54759cb3a959dc69a2d9f46c5e4ce5a18
hviezca <hviezca@my.gcu.edu>
2022-02-26 08:31:14 -0700

    Main Page - Now Available

--- BTEWebQuest/src/main/java/com/btewebquest/BteWebQuestApplication.java
+++ BTEWebQuest/src/main/java/com/btewebquest/BteWebQuestApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BteWebQuestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		System.out.println("Hello World");
 		SpringApplication.run(BteWebQuestApplication.class, args);
 	}
 
